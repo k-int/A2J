a813a690097e49730ac8d76538117453449f55ff
Ian Ibbotson <ianibbo@gmail.com>
2018-01-27 09:04:00 +0000

    updated

---
+++ src/main/groovy/com/k_int/a2j/ProtocolAssociation.groovy
@@ -0,0 +1,80 @@
+package com.k_int.a2j;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.SocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.*;
+import com.k_int.codec.runtime.BERInputStream
+import com.k_int.codec.runtime.BEROutputStream
+
+public class ProtocolAssociation<RootCodecClass, RootTypeClass> extends Thread {
+
+  private int server_status = 0; // 0=Idle, 1=Started, 2=Stop Requested, 3=Stopped
+  private Runnable server = null;
+  private Socket socket = null;
+  private RootCodecClass root_codec;
+  private InputStream incoming_data = null;
+  private OutputStream outgoing_data = null;
+
+  
+  public ProtocolAssociation(Socket socket, RootCodecClass root_codec) throws IOException {
+    root_codec = root_codec;
+    this.socket = socket;
+    try {
+      incoming_data = s.getInputStream();
+      outgoing_data = s.getOutputStream();
+    }
+    catch( java.io.IOException e ) {
+      cat.error("Error constructing TargetEndpoint",e);
+    }
+
+  }
+
+  public void run() {
+
+    while(server_status==1) {
+      BERInputStream bds = new BERInputStream(incoming_data,charset_encoding);
+      try {
+        // PDU_type pdu = null;
+        // pdu = (PDU_type)codec.serialize(bds, pdu, false, "PDU");
+        // notifyAPDUEvent(pdu);
+      }
+      catch ( java.io.InterruptedIOException iioe ) {
+        cat.error("Processing java.io.InterruptedIOException, shut down association",iioe);
+        running = false;
+        try {
+          //sendClose(0, "Session Timeout");
+        }
+        catch ( java.io.IOException ioe ) {
+          // Don't worry, the peer might just have got the close PDU and shut things down ahead of us...
+        }
+        // notifyClose();
+        // No need to close socket, but we should notify all listeners...
+      }
+      catch ( java.io.IOException ioe ) {
+        // Client snapped connection somehow...
+        if(ioe.getMessage().equals("Connection Closed")) {
+          cat.debug("Connection Closed");
+        }
+        else {
+          cat.error("Processing java.io.IOException, shut down association", ioe);
+        }
+        running = false;
+        notifyClose();
+      }
+      catch ( Exception e ) {
+        cat.error("Processing exception : ",e);
+        e.printStackTrace();
+        running=false;
+      }
+    }
+  }
+
+}
+
--- src/main/groovy/com/k_int/a2j/ProtocolServer.groovy
+++ src/main/groovy/com/k_int/a2j/ProtocolServer.groovy
@@ -13,17 +13,15 @@ import java.util.*;
 
 public class ProtocolServer<RootCodecClass, RootTypeClass> {
 
-  private Selector selector;
-  private Map<SocketChannel,ProtocolEndpoint<RootCodecClass, RootTypeClass>> dataMapper;
-  private InetSocketAddress listenAddress;
+  private int socket_timeout = 300000;  // 300 second default timeout
   private int server_status = 0; // 0=Idle, 1=Started, 2=Stop Requested, 3=Stopped
+  private int server_port = 0;
   private Runnable server = null;
   private RootCodecClass root_codec;
 
-  public ProtocolServer(String address, int port, RootCodecClass root_codec) throws IOException {
-    listenAddress = new InetSocketAddress(address, port);
-    dataMapper = new HashMap<SocketChannel,ProtocolEndpoint<RootCodecClass, RootTypeClass>>();
+  public ProtocolServer(int port, RootCodecClass root_codec) throws IOException {
     root_codec = root_codec;
+    this.server_port = port;
   }
 
   public void start() throws Exception {
@@ -74,89 +72,26 @@ public class ProtocolServer<RootCodecClass, RootTypeClass> {
 
   // create server channel  
   private void startServer() throws IOException {
+    server_socket = new ServerSocket(server_port);
+    try {
+      while ( server_status==1 ) {
+        cat.debug("Waiting for connection");
+        Socket socket = (Socket)server_socket.accept();
 
-    System.out.println("startServer()");
+        if ( socket_timeout > 0 )
+          socket.setSoTimeout(socket_timeout);
 
-    this.selector = Selector.open();
-    ServerSocketChannel serverChannel = ServerSocketChannel.open();
-    serverChannel.configureBlocking(false);
-
-    // retrieve server socket and bind to port
-    serverChannel.socket().bind(listenAddress);
-    serverChannel.register(this.selector, SelectionKey.OP_ACCEPT);
-
-    System.out.println("Server started...");
-
-    while (server_status==1) {
-      // wait for events
-      System.out.println("selector.select()");
-      this.selector.select();
-
-      //work on selected keys
-      Iterator keys = this.selector.selectedKeys().iterator();
-      while (keys.hasNext()) {
-        SelectionKey key = (SelectionKey) keys.next();
-
-        // this is necessary to prevent the same key from coming up 
-        // again the next time around.
-        keys.remove();
-
-        if (!key.isValid()) {
-          continue;
-        }
-
-        if (key.isAcceptable()) {
-          this.accept(key);
-        }
-        else if (key.isReadable()) {
-          this.read(key);
-        }
+        ProtocolAssociation pa = new ProtocolAssociation<RootCodecClass,RootTypeClass>(socket,root_codec)
       }
+
+      server_socket.close();
     }
-    System.out.println("startServer complete");
-    synchronized (this) {
-      server_status=3;
-      notifyAll();
-    }
-  }
-
-  //accept a connection made to this channel's socket
-  private void accept(SelectionKey key) throws IOException {
-    ServerSocketChannel serverChannel = (ServerSocketChannel) key.channel();
-    SocketChannel channel = serverChannel.accept();
-    channel.configureBlocking(false);
-    Socket socket = channel.socket();
-    SocketAddress remoteAddr = socket.getRemoteSocketAddress();
-    System.out.println("Connected to: " + remoteAddr);
-
-    // register channel with selector for further IO
-    ProtocolEndpoint<RootCodecClass, RootTypeClass> pe = new ProtocolEndpoint<RootCodecClass, RootTypeClass>(root_codec)
-    dataMapper.put(channel, pe);
-    channel.register(this.selector, SelectionKey.OP_READ);
-  }
-    
-  //read from the socket channel
-  private void read(SelectionKey key) throws IOException {
-    SocketChannel channel = (SocketChannel) key.channel();
-    ByteBuffer buffer = ByteBuffer.allocate(1024);
-    int numRead = -1;
-    numRead = channel.read(buffer);
-
-    if (numRead == -1) {
-      this.dataMapper.remove(channel);
-      Socket socket = channel.socket();
-      SocketAddress remoteAddr = socket.getRemoteSocketAddress();
-      System.out.println("Connection closed by client: " + remoteAddr);
-      channel.close();
-      key.cancel();
-      return;
+    catch (java.io.IOException e)
+    {
+      e.printStackTrace();
     }
 
-    byte[] data = new byte[numRead];
-    System.arraycopy(buffer.array(), 0, data, 0, numRead);
-    // System.out.println("Got: " + new String(data));
-    ProtocolEndpoint pe = this.dataMapper.get(channel);
-    pe.incomingData(data);
   }
+
 }
 
